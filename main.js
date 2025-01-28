8810011567191d2c36f70fa10069f943e1808386
Edison Chen <chen.edison2003@gmail.com>
2025-01-28 15:33:21 -0800

    Animation class changed

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const ASSET_MANAGER = new AssetManager();
 ASSET_MANAGER.queueDownload("./PaladinSpriteSheet.png");
 ASSET_MANAGER.queueDownload("./PlaceHolderSheet.png");
 ASSET_MANAGER.queueDownload("./GoblinPlaceHolderSheet.png");
+ASSET_MANAGER.queueDownload("./OrcSpriteTransparent.png");
 
 ASSET_MANAGER.downloadAll(() => {
 	const canvas = document.getElementById("gameWorld");
@@ -12,7 +13,7 @@ ASSET_MANAGER.downloadAll(() => {
 	ctx.imageSmoothingEnabled = false;
 	gameEngine.init(ctx);
 	gameEngine.addEntity(new Paladin(gameEngine,50,50, ASSET_MANAGER.getAsset("./PlaceHolderSheet.png"), 17, 40, 16, 24, 100, 20, 10, 10));
-	gameEngine.addEntity(new Goblin(gameEngine, 150,150, ASSET_MANAGER.getAsset("./GoblinPlaceHolderSheet.png"), 17, 40, 16, 24, 100, 10, 20, 5));
+	gameEngine.addEntity(new Goblin(gameEngine, 150,150, ASSET_MANAGER.getAsset("./OrcSpriteTransparent.png"), 17, 40, 16, 24, 100, 10, 20, 5));
 	
 
 	    // Create and add scene manager AFTER adding other entities
